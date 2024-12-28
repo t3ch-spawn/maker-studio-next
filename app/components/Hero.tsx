ed696fc23a8ecb38b5ce0a646feeb47f1345eb6f
Tife <boluwatife1904@gmail.com>
2024-12-28 14:02:52 +0100

    removed markers

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
           trigger: heading.classList.contains("hero") ? "body" : heading,
           start: "top 70%",
           end: "bottom 70%",
-          markers: true,
+          // markers: true,
         },
       });
     });
